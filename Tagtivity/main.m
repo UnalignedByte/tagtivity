f85c7136cc3ca8f4252bf77b9c8b7df8500af9ac
Rafał Grodziński <rgrodzinski@mobiltek.pl>
2013-04-23 17:08:33 +0200

    Initial Commit

---
+++ Tagtivity/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tagtivity
+//
+//  Created by Rafał Grodziński on 23.04.2013.
+//  Copyright (c) 2013 UnalignedByte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
